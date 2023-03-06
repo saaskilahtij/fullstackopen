8de9cb8f35e162a10cc953774659c093319b0058
Johan Sääskilahti <johan.saaskilahti@student.oulu.fi>
2023-03-06 15:31:24 +0200

    Not done.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,51 @@
+import { useState } from 'react';
 
 
+
+
+
+// Lisää nimiä! 
 const App = () => {
   
   
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const addName = (event) => {
+    event.preventDefault();
+    const person = {
+      name: newName,
+    }
+    setPersons(person);
+    setNewName('');
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+
   return (
     <div>
-      <p>Default text</p>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
     </div>
   )
+
 }
 
-export default App;
+export default App
+
+
